d4bfe2b92b60390ee968056debe3cbeaca608024
harry <harmanmanchanda182@gmail.com>
2017-10-08 00:33:21 +0530

    Remove unnecessary slots!

--- src/components/AppRevolution.vue
+++ src/components/AppRevolution.vue
@@ -1,13 +1,13 @@
 <template>
   <blockquote>
     <h5>
-      <slot name="title">Revolution did not necessarily involve sanguinary strife. It was not a cult of bomb and pistol.</slot>
+      Revolution did not necessarily involve sanguinary strife. It was not a cult of bomb and pistol.
     </h5>
     <p>
-      <slot name="content">The people generally get accustomed to the established order of things and begin to tremble at the very idea of a change. It is this lethargical spirit that needs be replaced by the revolutionary spirit. The spirit of Revolution should always permeate the soul of humanity, so that the reactionary forces may not accumulate to check its eternal onward march. Old order should change, always and ever, yielding place to new, so that one good order may not corrupt the world. It is in this sense that we raise the shout "Long Live Revolution".</slot>
+      The people generally get accustomed to the established order of things and begin to tremble at the very idea of a change. It is this lethargical spirit that needs be replaced by the revolutionary spirit. The spirit of Revolution should always permeate the soul of humanity, so that the reactionary forces may not accumulate to check its eternal onward march. Old order should change, always and ever, yielding place to new, so that one good order may not corrupt the world. It is in this sense that we raise the shout "Long Live Revolution".
     </p>
     <cite>
-      <slot name="author">Bhagat Singh</slot>
+      Bhagat Singh
     </cite>
   </blockquote>
 </template>
--- src/components/AppWomenPower.vue
+++ src/components/AppWomenPower.vue
@@ -1,13 +1,13 @@
 <template>
   <blockquote>
     <h5>
-      <slot name="title">To call woman the weaker sex is a libel; it is man's injustice to woman.</slot>
+      To call woman the weaker sex is a libel; it is man's injustice to woman.
     </h5>
     <p>
-      <slot name="content">If by strength is meant brute strength, then, indeed, is woman less brute than man. If by strength is meant moral power, then woman is immeasurably man's superior. Has she not greater intuition, is she not more self-sacrificing, has she not greater powers of endurance, has she not greater courage? Without her, man could not be. If nonviolence is the law of our being, the future is with woman. Who can make a more effective appeal to the heart than woman?</slot>
+      If by strength is meant brute strength, then, indeed, is woman less brute than man. If by strength is meant moral power, then woman is immeasurably man's superior. Has she not greater intuition, is she not more self-sacrificing, has she not greater powers of endurance, has she not greater courage? Without her, man could not be. If nonviolence is the law of our being, the future is with woman. Who can make a more effective appeal to the heart than woman?
     </p>
     <cite>
-      <slot name="author">Mohandas Karamchand Gandhi</slot>
+      Mohandas Karamchand Gandhi
     </cite>
   </blockquote>
 </template>
