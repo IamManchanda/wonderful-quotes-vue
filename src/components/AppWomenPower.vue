65e49d8a8abda069b60b026cf42fec93c2be1e81
harry <harmanmanchanda182@gmail.com>
2017-10-08 00:15:10 +0530

    Switching b/w multiple components thanks to dyn components!

---
+++ src/components/AppWomenPower.vue
@@ -0,0 +1,24 @@
+<template>
+  <blockquote>
+    <h5>
+      <slot name="title">To call woman the weaker sex is a libel; it is man's injustice to woman.</slot>
+    </h5>
+    <p>
+      <slot name="content">If by strength is meant brute strength, then, indeed, is woman less brute than man. If by strength is meant moral power, then woman is immeasurably man's superior. Has she not greater intuition, is she not more self-sacrificing, has she not greater powers of endurance, has she not greater courage? Without her, man could not be. If nonviolence is the law of our being, the future is with woman. Who can make a more effective appeal to the heart than woman?</slot>
+    </p>
+    <cite>
+      <slot name="author">Mohandas Karamchand Gandhi</slot>
+    </cite>
+  </blockquote>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  h5 { color: #1779ba; }
+  p { font-size: 0.95rem;margin-bottom: 0.5rem; }
+  cite { font-weight: bold; }
+</style>
